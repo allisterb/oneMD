173fdada7f6f2d9749cd8bc9f6a49da4165386ff
Allister Beharry <allister.beharry@gmail.com>
2021-02-15 23:28:22 -0400

    Add TCLAP library. Print logo.

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,13 @@
 #include "Figlet.hh"
 #include "spdlog/spdlog.h"
 #include "spdlog/cfg/env.h" // support for loading levels from the environment variable
+#include "tclap/CmdLine.h"
+#include <tclap/ArgGroup.h>
+#include <tclap/DeferDelete.h>
 
 using namespace std;
 using namespace spdlog;
+using namespace TCLAP;
 
 int
 figlet_demo() {
@@ -38,6 +42,9 @@ void update ( int np, int nd, double pos[], double vel[], double f[],
 //****************************************************************************80
 
 int main ( int argc, char *argv[] )
+// Wrap everything in a try block.  Do this every time,
+    // because exceptions will be thrown for problems.
+    
 
 //****************************************************************************80
 //
@@ -100,6 +107,58 @@ int main ( int argc, char *argv[] )
 
   Figlet::standard.print("oneMD");
   info("Welcome to oneMD. The default simulator by jburkardt is now active.");
+
+  // Wrap everything in a try block.  Do this every time, 
+	// because exceptions will be thrown for problems.
+	try {  
+
+	// Define the command line object, and insert a message
+	// that describes the program. The "Command description message" 
+	// is printed last in the help text. The second argument is the 
+	// delimiter (usually space) and the last one is the version number. 
+	// The CmdLine object parses the argv array based on the Arg objects
+	// that it contains. 
+	TCLAP::CmdLine cmd("Command description message", ' ', "0.9");
+
+	// Define a value argument and add it to the command line.
+	// A value arg defines a flag and a type of value that it expects,
+	// such as "-n Bishop".
+	TCLAP::ValueArg<std::string> nameArg("n","name","Name to print",true,"homer","string");
+
+	// Add the argument nameArg to the CmdLine object. The CmdLine object
+	// uses this Arg to parse the command line.
+	cmd.add( nameArg );
+
+	// Define a switch and add it to the command line.
+	// A switch arg is a boolean argument and only defines a flag that
+	// indicates true or false.  In this example the SwitchArg adds itself
+	// to the CmdLine object as part of the constructor.  This eliminates
+	// the need to call the cmd.add() method.  All args have support in
+	// their constructors to add themselves directly to the CmdLine object.
+	// It doesn't matter which idiom you choose, they accomplish the same thing.
+	TCLAP::SwitchArg reverseSwitch("r","reverse","Print name backwards", cmd, false);
+
+	// Parse the argv array.
+	cmd.parse( argc, argv );
+
+	// Get the value parsed by each arg. 
+	std::string name = nameArg.getValue();
+	bool reverseName = reverseSwitch.getValue();
+
+	// Do what you intend. 
+	if ( reverseName )
+	{
+		std::reverse(name.begin(),name.end());
+		std::cout << "My name (spelled backwards) is: " << name << std::endl;
+	}
+	else
+		std::cout << "My name is: " << name << std::endl;
+
+
+	} catch (TCLAP::ArgException &e)  // catch exceptions
+	{ std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl; }
+
+  
 
   timestamp ( );
   cout << "\n";
