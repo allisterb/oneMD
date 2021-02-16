69d0d456cd1f6ef716f6c3c770d39cb491f11d8c
allisterb <allister.beharry@gmail.com>
2021-02-16 14:25:21 -0800

    Add some cmdline options. Use build command in tasks.json

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,7 @@
 #include "spdlog/spdlog.h"
 #include "spdlog/cfg/env.h" // support for loading levels from the environment variable
 #include "tclap/CmdLine.h"
-#include <tclap/ArgGroup.h>
-#include <tclap/DeferDelete.h>
+#include "tclap/UnlabeledValueArg.h"
 
 using namespace std;
 using namespace spdlog;
@@ -106,60 +105,23 @@ int main ( int argc, char *argv[] )
   double *vel;
 
   Figlet::standard.print("oneMD");
-  info("Welcome to oneMD. The default simulator by jburkardt is now active.");
-
-  // Wrap everything in a try block.  Do this every time, 
-	// because exceptions will be thrown for problems.
 	try {  
-
-	// Define he command line object, and insert a message
-	// that describes the program. The "Command description message" 
-	// is printed last in the help text. The second argument is the 
-	// delimiter (usually space) and the last one is the version number. 
-	// The CmdLine object parses the argv array based on the Arg objects
-	// that it contains. 
-	TCLAP::CmdLine cmd("Command description msage", ' ', "0.9");
-
-	// Define a value argument and add it to the command line.
-	// A value arg defines a flag and a type of value that it expects,
-	// such as "-n Bishop".
-	TCLAP::ValueArg<std::string> nameArg("n","name","Name to print",true,"homer","string");
-
-	// Add the argument nameArg to the CmdLine object. The CmdLine object
-	// uses this Arg to parse the command line.
-	cmd.add( nameArg );
-
-	// Define a switch and add it to the command line.
-	// A switch arg is a boolean argument and only defines a flag that
-	// indicates true or false.  In this example the SwitchArg adds itself
-	// to the CmdLine object as part of the constructor.  This eliminates
-	// the need to call the cmd.add() method.  All args have support in
-	// their constructors to add themselves directly to the CmdLine object.
-	// It doesn't matter which idiom you choose, they accomplish the same thing.
-	TCLAP::SwitchArg reverseSwitch("r","reverse","Print name backwards", cmd, false);
-
-	// Parse the argv array.
-	cmd.parse( argc, argv );
-
-	// Get the value parsed by each arg. 
-	std::string name = nameArg.getValue();
-	bool reverseName = reverseSwitch.getValue();
-
-	// Do what you intend. 
-	if ( reverseName )
-	{
-		std::reverse(name.begin(),name.end());
-		std::cout << "My name (spelled backwards) is: " << name << std::endl;
+    CmdLine cmd("oneMD data-parallel molecular dynamics simulator.", ' ', "0.1");
+    UnlabeledValueArg<string>  simArg("simulator","Name of simulator to run.",true,"cpu","string", cmd);
+    UnlabeledValueArg<int> dimsArg("dimensions","Number of dimensions for simulation from 1 - 3.",true, 1,"integer", cmd);
+    SwitchArg debugArg("d","debug","Enable debug-level logging.", cmd, false);
+    
+    cmd.parse( argc, argv );
+    auto debugLog = debugArg.getValue();
+    
+    if (debugLog) {
+      set_level(level::debug);
+      info("Debug-level logging enabled.");
+    }
 	}
-	else
-		std::cout << "My name is: " << name << std::endl;
-
-
-	} catch (TCLAP::ArgException &e)  // catch exceptions
-	{ std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl; }
-
-  
-
+  catch (ArgException &e) { 
+    std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl; 
+  }
   timestamp ( );
   cout << "\n";
   cout << "MD\n";
