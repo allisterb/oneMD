f47ac9414b0676d4dbe3c997c126d1e605236ed9
allisterb <allister.beharry@gmail.com>
2021-02-19 16:04:47 -0800

    Add libxdrfile as shared library.

--- src/Simulator.cpp
+++ src/Simulator.cpp
@@ -1,5 +1,5 @@
 #include "Simulator.hh"
-
+#include "xdrfile.h"
 Simulator::Simulator(const string _name, const int _nd, const int _np, const int _ts, const float _ts_delta, const Device _device) :
   name(_name),
   nd(_nd),
